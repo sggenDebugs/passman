9991423b00bd49297fc7d114266baca84fbcef78
sggenDebugs <ig052417@gmail.com>
2025-09-07 02:52:08 +0800

    test: add test script for UserRepository

---
+++ passman/src/test/java/com/sggen/passman/repository/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.sggen.passman.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryTest {
+
+	@Test
+	void testUserRepository() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAuthData() {
+		fail("Not yet implemented");
+	}
+
+}
