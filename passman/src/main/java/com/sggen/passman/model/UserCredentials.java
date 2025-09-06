4c2562974c36d262f1eee8eba5bde96fd26da7ac
sggenDebugs <ig052417@gmail.com>
2025-09-07 00:58:37 +0800

    code: modify UserCredentials.java to standalone public record
    Signed-off-by: sggenDebugs <ig052417@gmail.com>

--- passman/src/main/java/com/sggen/passman/model/UserCredentials.java
+++ passman/src/main/java/com/sggen/passman/model/UserCredentials.java
@@ -1,9 +1,4 @@
 package com.sggen.passman.model;
 
-public class UserCredentials {
-	public record userCredentials(
-			String username,
-			String password_hash,
-			String password_salt
-			) {}
+public record UserCredentials(String username, String password_hash, String password_salt) {
 }
